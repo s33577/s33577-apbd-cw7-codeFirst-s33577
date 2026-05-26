ba26a71643e340f76e1994dbb04e8b165a71d32b
Andrew Kotowiecki <s33577@pjwstk.edu.pl>
2026-05-27 00:49:55 +0200

    Created the Tasks.

--- FormatTEST/FormatTEST/Services/IDbService.cs
+++ FormatTEST/FormatTEST/Services/IDbService.cs
@@ -1,6 +1,11 @@
+using FormatTEST.DTOs;
+
 namespace FormatTEST.Services;
 
 public interface IDbService
 {
+    Task<IEnumerable<PCResponseDTO>> GetAllPCsAsync();
+    Task<PCResponseDTO> CreatePCAsync(CreatePCRequestDTO request);
+    Task<bool> DeletePCAsync(int id);
     
 }
