57bb079f6e6670bb027d4dea9acc998fbcddf1ef
Andrew Kotowiecki <s33577@pjwstk.edu.pl>
2026-05-27 00:46:41 +0200

    DTOs created

---
+++ FormatTEST/FormatTEST/DTOs/CreatePCRequestDTO.cs
@@ -0,0 +1,11 @@
+namespace FormatTEST.DTOs;
+
+public class CreatePCRequestDTO
+{
+    public string Name { get; set; }
+    public float Weight { get; set; }
+    public int Warranty { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public int Stock { get; set; }
+    
+}
---
+++ FormatTEST/FormatTEST/DTOs/PCResponseDTO.cs
@@ -0,0 +1,12 @@
+namespace FormatTEST.DTOs;
+
+public class PCResponseDTO
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public float Weight { get; set; }
+    public int Warranty { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public int Stock { get; set; }
+    
+}
