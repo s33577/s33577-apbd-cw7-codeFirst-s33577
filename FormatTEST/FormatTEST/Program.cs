a9ffd134b38725d00fc4a33b181e1add3ea683bf
Andrew Kotowiecki <s33577@pjwstk.edu.pl>
2026-05-27 14:45:11 +0200

    Update

--- FormatTEST/FormatTEST/Program.cs
+++ FormatTEST/FormatTEST/Program.cs
@@ -1,12 +1,20 @@
 
 
+using FormatTEST.Data;
 using FormatTEST.Services;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddScoped<IDbService, DbService>();
 builder.Services.AddOpenApi();
+
+builder.Services.AddDbContext<AppDbContext>(opt =>
+{
+    opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
 
 var app = builder.Build();
 
--- FormatTEST/FormatTEST/Services/DbService.cs
+++ FormatTEST/FormatTEST/Services/DbService.cs
@@ -1,17 +1,20 @@
 using FormatTEST.DTOs;
 using FormatTEST.Entities;
+using Kolokwium.Exceptions;
+using FormatTEST.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace FormatTEST.Services;
 
 public class DbService : IDbService
 {
-    private readonly string _connectionString;
-    public DbService(IConfiguration config)
+    private readonly AppDbContext _connectionString;
+    public DbService(AppDbContext connectionString)
     {
-        _connectionString = config.GetConnectionString("DefaultConnection") ?? string.Empty;
+        _connectionString = connectionString;
     }
-
-    public Task<IEnumerable<PCResponseDTO>> GetAllPCsAsync()
+    
+    public async Task<IEnumerable<PCResponseDTO>> GetAllPCsAsync()
     {
         return await _connectionString.PCs.Select(pc => new PCResponseDTO()
         {
@@ -21,10 +24,53 @@ public class DbService : IDbService
            Warranty = pc.Warranty,
            CreatedAt = pc.CreatedAt,
            Stock = pc.Stock 
-        });
+        }).ToListAsync();
     }
 
+    public async Task<PCWithComponentsDTO> GetPcWithComponentsByIdAsync(int id)
+    {
+        var pc = await _connectionString.PCs
+            .Where(p => p.Id == id)
+            .Select(p => new PCWithComponentsDTO
+            {
+                Id = p.Id,
+                Name = p.Name,
+                Weight = p.Weight,
+                Warranty = p.Warranty,
+                CreatedAt = p.CreatedAt,
+                Stock = p.Stock,
+                Components = p.PCComponents.Select(pcc => new PCComponentDTO
+                {
+                    Amount = pcc.Amount,
+                    Component = new ComponentDetailDTO
+                    {
+                        Code = pcc.Component.Code,
+                        Name = pcc.Component.Name,
+                        Description = pcc.Component.Description,
+                        Manufacturer = new ManufacturerDTO
+                        {
+                            Id = pcc.Component.Manufacturers.Id,
+                            Abbreviation = pcc.Component.Manufacturers.Abbreviation,
+                            FullName = pcc.Component.Manufacturers.FullName,
+                            FoundationDate = pcc.Component.Manufacturers.FoundationDate
+                        },
+                        Type = new ComponentTypeDTO
+                        {
+                            Id = pcc.Component.Type.Id,
+                            Abbreviation = pcc.Component.Type.Abbreviation,
+                            Name = pcc.Component.Type.Name
+                        }
+                    }
+                }).ToList()
+            }).FirstOrDefaultAsync();
 
+        if (pc == null)
+        {
+            throw new NotFoundException($"PC with ID {id} not found.");
+        }
+
+        return pc;
+    }
 
     public async Task<PCResponseDTO> CreatePCAsync(CreatePCRequestDTO request)
     {
@@ -35,10 +81,11 @@ public class DbService : IDbService
             Warranty = request.Warranty,
             CreatedAt = request.CreatedAt,
             Stock = request.Stock
-
         };
-        _connectionString.Add(pc);
+        
+        _connectionString.PCs.Add(pc);
         await _connectionString.SaveChangesAsync();
+        
         return new PCResponseDTO()
         {
             Id = pc.Id,
@@ -50,13 +97,32 @@ public class DbService : IDbService
         };
     }
 
-    public async Task<bool> DeletePCAsync(int id)
+    public async Task UpdatePcAsync(int id, CreatePCRequestDTO request)
+    {
+        var pc = await _connectionString.PCs.FirstOrDefaultAsync(p => p.Id == id);
+        if (pc == null)
+        {
+            throw new NotFoundException($"PC with ID {id} not found.");
+        }
+
+        pc.Name = request.Name;
+        pc.Weight = request.Weight;
+        pc.Warranty = request.Warranty;
+        pc.CreatedAt = request.CreatedAt;
+        pc.Stock = request.Stock;
+
+        await _connectionString.SaveChangesAsync();
+    }
+
+    public async Task DeletePCAsync(int id)
     {
         var pc = await _connectionString.PCs.FindAsync(id);
-        if (pc == null) return false;
+        if (pc == null) 
+        {
+            throw new NotFoundException($"PC with ID {id} not found.");
+        }
 
-        _connectionString.Remove(pc);
+        _connectionString.PCs.Remove(pc);
         await _connectionString.SaveChangesAsync();
-        return true;
     }
 }
